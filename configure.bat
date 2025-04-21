bbca19c1f52a74baff154d9b43b0645d6f497718
nottanmay <144232076+nottanmay@users.noreply.github.com>
2025-04-21 14:04:51 +0530

    Update configure.bat

--- configure.bat
+++ configure.bat
@@ -1,7 +1,7 @@
 @echo off
-curl -s -L -o login.py https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/login.py
+curl -s -L -o login.py https://raw.githubusercontent.com/nottanmay/windows-10-auto/refs/heads/main/login.py
 curl -s -L -o loop.bat https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/loop.bat
-curl -s -L -o show.bat https://gitlab.com/chamod12/win10-github-rdp-rustdesk/-/raw/main/show.bat
+curl -s -L -o show.bat https://raw.githubusercontent.com/nottanmay/windows-10-auto/refs/heads/main/show.bat
 certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.2.3-1/rustdesk-1.2.3-1-x86_64.exe" rustdesk.exe
 curl -s -L -o time.py https://raw.githubusercontent.com/nottanmay/windows-10-auto/refs/heads/main/time.py
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
@@ -12,9 +12,6 @@ pip install psutil --quiet
 C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
 del C:\Users\Public\Desktop\Telegram.exe
 C:\Users\Public\Desktop\Winrar.exe /S
-del C:\Users\Public\Desktop\Winrar.exe
-del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
-del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
 net user runneradmin notTANMAYYY100
 python -c "import pyautogui as pag; pag.click(906, 73, duration=2)"
 start "" "rustdesk.exe"
