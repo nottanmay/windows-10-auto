cb61cc3d6d8f23da809a9ea1f47868dd1b1df52d
nottanmay <144232076+nottanmay@users.noreply.github.com>
2025-04-21 14:02:07 +0530

    Create show.bat

---
+++ show.bat
@@ -0,0 +1,12 @@
+@echo off
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(143, 487, duration=5)"
+python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
+python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
+python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
+echo ..........................................................
+echo ........ Made by nottanmayyy (nottanmaydev@gmail.com).....
+echo ..........................................................
+echo Your Device Name: %username%@%computername%
+echo User Name: runneradmin
+echo User Pass: notTANMAYYY100
